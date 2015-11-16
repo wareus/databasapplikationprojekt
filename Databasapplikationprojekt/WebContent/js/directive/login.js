8f3b544eec9254377d2052311a80edb9ebe43a3c
nbt09rem <nbt09rem@07590-STUD-D.HIG-AD.hig.se>
2015-11-16 15:40:44 +0100

    frontend login directive

--- Databasapplikationprojekt/WebContent/js/directive/login.js
+++ Databasapplikationprojekt/WebContent/js/directive/login.js
@@ -1,12 +1,20 @@
-angular.module('loginDirective', [ 'login' ])
-.directive('login', function() {
-	
-	console.log(123);
-	
+angular.module('loginDirective', [ 'login' ]).directive('login', function(Login) {
+
 	return {
 		restrict : 'E',
-		templateUrl : 'html/directive/login.html'
+		templateUrl : 'html/directive/login.html',
 
+		controller : [ '$scope', function($scope) {
+			$scope.loginClick = function()
+			{
+				Login.save($scope.login).$promise.catch(function(response) {
+				    if(response.status == 401) alert("please retry to login agin", "Login failed");
+				});
+				
+				
+				
+			}
+		} ]
 	}
 
 });
