b58f3f09b498ed8e979f84a8a61104567676bb50
alexandereriksson90 <alexandereriksson90@live.com>
2015-11-16 14:13:22 +0100

    Inlägg av bashemsida!

--- Databasapplikationprojekt/WebContent/js/route.js
+++ Databasapplikationprojekt/WebContent/js/route.js
@@ -1,6 +1,10 @@
 angular.module("ArnProjectPlanner", ["ngRoute", "test"]).config(function($routeProvider) {
     $routeProvider
 	    .when("/", {
+	    	templateUrl : "html/views/Bashemsida.html",
+	    	controller  : "testController"
+	    })
+	    .when("/test", {
 	    	templateUrl : "html/views/test.html",
 	    	controller  : "testController"
 	    })
