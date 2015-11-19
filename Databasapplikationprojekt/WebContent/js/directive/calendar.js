f0d9b58c7dd1230a5370c8678ccef32f12873e36
alexandereriksson90 <alexandereriksson90@live.com>
2015-11-19 13:33:36 +0100

    fixat funktioner i kalendern

--- Databasapplikationprojekt/WebContent/js/directive/calendar.js
+++ Databasapplikationprojekt/WebContent/js/directive/calendar.js
@@ -1,39 +1,51 @@
-angular.module('calendarDirective', ['ui.calendar'])
-.directive('calendar', function() {
-	
-	
-	
-	return {
-		restrict : 'E',
-		templateUrl : 'html/directive/calendar.html',
-		
-		controller : [ '$scope', function($scope) {
-				
-			
-					console.log(22);
-					
-					$scope.eventSources = [];
-					
-					$scope.uiConfig = {
-						      calendar:{
-						        height: 450,
-						        editable: true,
-						        header:{
-						          left: 'month basicWeek ',
-						          center: 'title',
-						          right: 'today prev,next'
-						        },
-						        dayClick: $scope.alertEventOnClick,
-						        eventDrop: $scope.alertOnDrop,
-						        eventResize: $scope.alertOnResize
-						      }
-						    };
-					
-					
-					
-					
-				}
-			]
-	}
+angular
+		.module('calendarDirective', [ 'ui.calendar' ])
+		.directive(
+				'calendar',
+				function() {
 
-});
+					return {
+						restrict : 'E',
+						templateUrl : 'html/directive/calendar.html',
+
+						controller : [
+								'$scope',
+								function($scope) {
+
+									console.log(22);
+
+									$scope.eventSources = [];
+
+									$scope.uiConfig = {
+										
+										calendar : {
+											height : 450,
+											editable : true,
+											selectable : true,
+											weekNumbers: true,
+											header : {
+												
+												left : 'month basicWeek basicQuarter',
+												center : 'title',
+												right : 'today prev,next'
+											},
+											
+											views : {
+												basicQuarter : {
+													type : 'basic',
+													duration : {
+														months : 3
+													},
+													buttonText : 'quarter'
+												}
+											},
+											dayClick : $scope.alertEventOnClick,
+											eventDrop : $scope.alertOnDrop,
+											eventResize : $scope.alertOnResize
+										}
+									};
+
+								} ]
+					}
+
+				});
