6a0fce49dfcc88abfcb6ce99b1a84f95872b50b3
nbt09rem <nbt09rem@07590-STUD-D.HIG-AD.hig.se>
2015-11-23 15:57:46 +0100

    modal test

--- Databasapplikationprojekt/WebContent/js/errorHandler.js
+++ Databasapplikationprojekt/WebContent/js/errorHandler.js
@@ -1,12 +1,21 @@
-angular.module('errorHandler', []).config(["$httpProvider", function ($httpProvider) {
+angular.module('errorHandler', ['mm.foundation']).config(["$httpProvider", function ($httpProvider) {
 
-	$httpProvider.interceptors.push(function($q) {
+	$httpProvider.interceptors.push(function($q, $modal) {
 
 			
 		return{
 			       'responseError': function(response) {
 			        if(response.status === 401)
 			        {
+			        	var modalInstance = $modal.open({
+					        templateUrl: 'html/modal/loginModal.html',
+					        controller: function($scope,$modalInstance)
+					        {
+					        	 $scope.cancel = function () {
+					        		 $modalInstance.dismiss('cancel');
+					        	};
+					        }
+					    });
 			        }
 			        return $q.reject(response);
 			      }
