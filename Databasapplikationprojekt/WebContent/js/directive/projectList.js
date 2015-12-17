4081121aafbb89291f6615be5a37108fb8547707
wareus <rien90@hotmail.com>
2015-12-17 14:57:32 +0100

    UI for update category added

--- Databasapplikationprojekt/WebContent/js/directive/projectList.js
+++ Databasapplikationprojekt/WebContent/js/directive/projectList.js
@@ -6,27 +6,31 @@ angular.module('projectListDirective', ['dto']).directive('projectList', functio
 		templateUrl : 'html/directive/projectList.html',
 
 		controller : [ '$scope', function($scope) {
-			
+			$scope.categories = CATEGORIES;
 			$scope.projectList = {
-					select:function(id)
+				selectCategory:function(id,category)
+				{
+					new Project({id:id,kategori:category}).$save();
+				},
+				select:function(id)
+				{
+					Project.select(id);
+				},
+				delete:function(id)
+				{
+					new Project({id:id}).$remove(function()
 					{
-						Project.select(id);
-					},
-					delete:function(id)
-					{
-						new Project({id:id}).$remove(function()
-						{
-							$scope.projectList.update();
-							
-						});
-					},
-					update:function()
-					{
-						Project.yourProjects(function(data) {
-							$scope.projectList.data = data;
-							
-						});
-					}
+						$scope.projectList.update();
+						
+					});
+				},
+				update:function()
+				{
+					Project.yourProjects(function(data) {
+						$scope.projectList.data = data;
+						console.log($scope.projectList);
+					});
+				}
 			};
 			$scope.projectList.update();
 		} ]
